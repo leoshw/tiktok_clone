04a77cd1a9e118b4522471b143f2a452737f12bb
심현웅 <hyunwoong-shim@Leos-MacBook-Pro-2.local>
2025-06-05 10:54:00 +0900

    First project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TiktokApp());
+}
+
+class TiktokApp extends StatelessWidget {
+  const TiktokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
